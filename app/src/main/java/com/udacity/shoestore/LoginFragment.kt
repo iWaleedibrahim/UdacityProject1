97ff0d34fc913af62705cbc85a65eb4a9a6028d7
Waleed Ibrahim <iwaleedibrahim@gmail.com>
2022-09-24 04:16:14 +0200

    added all fragments and navigation as well and made the login ui

--- app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import com.udacity.shoestore.databinding.FragmentLoginBinding
@@ -13,23 +12,14 @@ import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
-
-
-    // TODO - WHAT IS THE PURPOSE OF THIS onCreate Method ?
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding: FragmentLoginBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_login, container, false)
-
-        binding.welcome_login_text.setOnClickListener (
-            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        binding.loginButton.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment))
         return binding.root
     }
-
 }
