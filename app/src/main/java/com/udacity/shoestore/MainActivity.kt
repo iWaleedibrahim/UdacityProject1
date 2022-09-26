791d91dfb7713be4b6952f9c2351f57a64e5eb0c
Waleed Ibrahim <iwaleedibrahim@gmail.com>
2022-09-26 22:37:09 +0200

    .

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.udacity.shoestore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,13 +15,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }
+//    navController.addOnDestinationChangedListener { navController, destination, arguments ->
+//        title = when (destination.id) {
+//            R.id.loginFragment -> "Login"
+//            R.id.welcomeFragment -> "Welcome"
+//            R.id.instructionsFragment -> "Instructions"
+//            R.id.listingFragment -> "List of shoes"
+//            R.id.detailFragment -> "Shoe"
+//            else -> "ShoeStore"
+//        }
+//    }
+
 }
 
 
